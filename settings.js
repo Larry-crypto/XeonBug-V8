04af6c329116f22297c2097e9f598f0219642ba4
Larry-crypto <jiaamariee@outlook.com>
2024-10-15 13:59:33 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2347048082848"
+global.ownername = "Larry"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
